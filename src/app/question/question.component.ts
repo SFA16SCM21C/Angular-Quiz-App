ff87cd1372e75d49b7d56675e4d610e92ef40dd9
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-30 20:56:49 -0500

    question componenet added

---
+++ src/app/question/question.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../service/question.service';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+
+  public name: string="";
+  constructor(private questionService : QuestionService) { }
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem("name")!;
+    this.getAllQuestions();
+  }
+
+  getAllQuestions() {
+    this.questionService.getquestions()
+    .subscribe(res => {
+      console.log(res.questions);
+    })
+  }
+}
