6bf5ee4472042c29ec46fc72ba73f26a44522e60
Saloni Chauhan <schauhan1@hawk.iit.edu>
2023-07-30 22:07:16 -0500

    final touches to timers and question flow

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../service/question.service';
+import { interval, queueScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-question',
@@ -9,17 +10,79 @@ import { QuestionService } from '../service/question.service';
 export class QuestionComponent implements OnInit {
 
   public name: string="";
+  public questionList: any =[];
+  public currentQuestion: number = 0;
+  public points: number = 0;
+  counter = 60;
+  correctAnswer: number = 0;
+  inCorrectAnswer: number = 0;
+  interval$: any;
+
   constructor(private questionService : QuestionService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
     this.getAllQuestions();
+    this.startCounter();
   }
 
   getAllQuestions() {
     this.questionService.getquestions()
     .subscribe(res => {
-      console.log(res.questions);
+      this.questionList = res.questions;
     })
+  }
+
+  nextQuestion() {
+    this.currentQuestion++;
+  }
+
+  previousQuestion() {
+    this.currentQuestion--;
+  }
+
+  checkAnswer(currentQno: number, option: any) {
+    if (option.correct) {
+      this.points += 10;
+      this.correctAnswer++;
+      this.currentQuestion++;
+    } else {
+      this.points -= 10;
+      this.inCorrectAnswer++;
+      this.currentQuestion++;
+    }
+  }
+
+  startCounter() {
+    this.interval$ = interval(1000)
+    .subscribe(val => {
+      this.counter--;
+      if (this.counter === 0) {
+        this.currentQuestion++;
+        this.counter = 60;
+        this.points -= 10;
+      }
+    });
+    setTimeout(() => {
+      this.interval$.unsubscribe()
+    }, 600000)
+  }
+
+  stopCounter() {
+    this.interval$.unsubscribe();
+    this.counter = 0;
+  }
+
+  resetCounter() {
+    this.startCounter();
+    this.counter = 60;
+    this.startCounter();
+  }
+
+  resetQuiz() {
+    this.resetCounter();
+    this.getAllQuestions();
+    this.points = 0;
+    this.counter = 60;
   }
 }
